ef2e74cad05c6340e0f5d92103175063d7f5e977
KO-YAMAKO <ko.foot1103@gmail.com>
2018-04-23 01:46:10 +0900

    set up project

---
+++ sample_textfield/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample_textfield
+//
+//  Created by 山浦功 on 2018/04/23.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
