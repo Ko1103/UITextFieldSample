74492d5544e70370bdc38cd64876231678107cb2
KO-YAMAKO <ko.foot1103@gmail.com>
2018-04-23 12:22:37 +0900

    add outlet connect

--- sample_textfield/ViewController.swift
+++ sample_textfield/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var thirdTextField: UITextField!
+    @IBOutlet weak var fourthTextField: UITextField!
+    
+    fileprivate var activeTextField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
